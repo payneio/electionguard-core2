991639b36017d9a97e27d271cb51d4610dc0f8cf
Paul Payne <paul@payne.io>
2026-03-30 11:29:17 -0700

    test(tally): add v2.1 challenged ballot nonce reveal verification test

--- test/electionguard/test_tally_v21.cpp
+++ test/electionguard/test_tally_v21.cpp
@@ -3,6 +3,7 @@
 #include <electionguard/elgamal.hpp>
 #include <electionguard/group.hpp>
 #include <electionguard/hash.hpp>
+#include <electionguard/nonces.hpp>
 
 using namespace electionguard;
 using namespace std;
@@ -110,4 +111,97 @@ TEST_CASE("v2.1 decryption proof: full round-trip with single guardian")
         a->clone(), b->clone(), challenge->clone(), v->clone());
 
     CHECK(proof->isValid(*ct, *keypair->getPublicKey(), *M, *H_E, 0, 0));
+}
+
+TEST_CASE("v2.1 challenged ballot: decrypt nonce, re-derive nonces, verify")
+{
+    auto keypair = ElGamalKeyPair::fromSecret(TWO_MOD_Q());
+    auto kHatPair = ElGamalKeyPair::fromSecret(*rand_q());
+    auto H_E = rand_q();
+    auto id_B = rand_q();
+    auto xi_B = rand_q(); // ballot nonce
+
+    auto H_I = compute_selection_encryption_id(H_E.get(), id_B.get());
+
+    // Encrypt ballot nonce with K_hat
+    auto nonceCt = HashedElGamalCiphertext::encryptBallotNonce(
+        xi_B.get(), kHatPair->getPublicKey(), H_I.get());
+
+    // Encrypt a selection with derived nonce
+    uint64_t contestIdx = 0, selIdx = 0;
+    auto derived_nonce = derive_selection_nonce(H_I.get(), contestIdx, selIdx, xi_B.get());
+    auto ct = elgamalEncrypt(1UL, *derived_nonce, *keypair->getPublicKey());
+
+    // Guardians decrypt the ballot nonce
+    auto decrypted_xi_B = nonceCt->decryptBallotNonce(
+        kHatPair->getSecretKey(), H_I.get());
+    REQUIRE((*decrypted_xi_B == *xi_B));
+
+    // Verifier re-derives nonces and checks ciphertext
+    auto re_derived = derive_selection_nonce(H_I.get(), contestIdx, selIdx, decrypted_xi_B.get());
+    auto re_ct = elgamalEncrypt(1UL, *re_derived, *keypair->getPublicKey());
+    CHECK((*re_ct->getPad() == *ct->getPad()));
+    CHECK((*re_ct->getData() == *ct->getData()));
+}
+
+TEST_CASE("v2.1 contest data commitment hash: d_i = H(H_I; 0x32, ...)")
+{
+    auto H_I = rand_q();
+    auto keypair = ElGamalKeyPair::fromSecret(TWO_MOD_Q());
+    auto xi_B = rand_q();
+    uint64_t contestIndex = 0;
+
+    // Encrypt contest data
+    vector<uint8_t> contestData(32, 0xCD);
+    auto encrypted = HashedElGamalCiphertext::encryptContestData(
+        contestData, keypair->getPublicKey(), H_I.get(), contestIndex, xi_B.get());
+
+    // Commitment
+    auto u = rand_q();
+    auto a_i = g_pow_p(*u);
+    auto b_i = pow_mod_p(*encrypted->getPad(), *u);
+    auto m_i = pow_mod_p(*encrypted->getPad(), *keypair->getSecretKey());
+
+    auto d_i = DecryptionProof::computeContestDataCommitmentHash(
+        H_I.get(), contestIndex, 1,
+        encrypted->getPad(), encrypted->getData(), encrypted->getMac(),
+        a_i.get(), b_i.get(), m_i.get(), {1});
+    REQUIRE(d_i != nullptr);
+
+    // Deterministic
+    auto d_i_2 = DecryptionProof::computeContestDataCommitmentHash(
+        H_I.get(), contestIndex, 1,
+        encrypted->getPad(), encrypted->getData(), encrypted->getMac(),
+        a_i.get(), b_i.get(), m_i.get(), {1});
+    CHECK((*d_i == *d_i_2));
+}
+
+TEST_CASE("v2.1 contest data decryption challenge: c = H_q(H_I; 0x33, ...)")
+{
+    auto H_I = rand_q();
+    auto keypair = ElGamalKeyPair::fromSecret(TWO_MOD_Q());
+    auto xi_B = rand_q();
+    uint64_t contestIndex = 0;
+
+    vector<uint8_t> contestData(32, 0xCD);
+    auto encrypted = HashedElGamalCiphertext::encryptContestData(
+        contestData, keypair->getPublicKey(), H_I.get(), contestIndex, xi_B.get());
+
+    auto u = rand_q();
+    auto a = g_pow_p(*u);
+    auto b = pow_mod_p(*encrypted->getPad(), *u);
+    auto M = pow_mod_p(*encrypted->getPad(), *keypair->getSecretKey());
+
+    auto challenge = DecryptionProof::computeContestDataDecryptionChallenge(
+        H_I.get(), contestIndex,
+        encrypted->getPad(), encrypted->getData(), encrypted->getMac(),
+        a.get(), b.get(), M.get());
+    REQUIRE(challenge != nullptr);
+
+    // Deterministic
+    auto challenge2 = DecryptionProof::computeContestDataDecryptionChallenge(
+        H_I.get(), contestIndex,
+        encrypted->getPad(), encrypted->getData(), encrypted->getMac(),
+        a.get(), b.get(), M.get());
+    CHECK((*challenge == *challenge2));
 }
